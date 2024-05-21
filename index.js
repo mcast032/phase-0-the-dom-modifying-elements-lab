5bcb13c5c4bb6faa58681e3e869d970f965cce34
Mario Castro <mcast032@gmail.com>
2024-05-21 09:58:49 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+
+const mainElement = document.querySelector('main#main');
+if (mainElement){
+    mainElement.remove();
+}
+
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+
+newHeader.textContent = 'Mario is the champion'; 
